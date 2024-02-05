4b3e1acea5bbb663a1ee5ce2ae65a26a29dcfeef
Park Juyeon <18cwceark@gmail.com>
2024-02-05 14:11:39 +0900

    Title: 다양하게 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240205/다양하게 출력/print-in-variety.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+		sb.append("Total days in Year\n");
+		sb.append("365\n");
+		sb.append("Circumference rate\n");
+		sb.append("3.1415926535\n");
+		System.out.println(sb);
+    }
+}
