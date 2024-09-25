76a9bcf52d43562bcb01f8be0bd58d3643dcde93
Park Juyeon <18cwceark@gmail.com>
2024-09-25 16:42:49 +0900

    Title: 두 사람, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/두 사람/two-person.py
+++ 240925/두 사람/two-person.py
@@ -4,7 +4,7 @@ blist = input().split()
 a_age = int(alist[0])
 b_age = int(blist[0])
 
-if (a_age >= 19 and alist[1] == 'M') or (b_age >= 19 and blist[1] == 'F'):
+if (a_age >= 19 and alist[1] == 'M') or (b_age >= 19 and blist[1] == 'M'):
     print(1)
 else:
     print(0)
